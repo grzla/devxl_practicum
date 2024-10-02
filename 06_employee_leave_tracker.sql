5a8a9bcab1c19a1e15d6931dfbe8e9a0d3395deb
grzla <greg.j.lundberg@gmail.com>
2024-10-02 19:09:28 -0400

    06 employee leave tracker.sql

--- 06_employee_leave_tracker.sql
+++ 06_employee_leave_tracker.sql
@@ -1,5 +1,12 @@
-select e.email, l.days_taken
+select e.email, 
+    sum(l.days_taken) as leave_days_taken,
+    case 
+        when sum(l.days_taken) <= 20 then 'Within Limit'
+        else 'Exceeded'
+    end as leave_status
 from leave_records l
 join employees e 
 on e.id = l.employee_id
 where year(l.leave_dt) = 2023
+group by e.email
+order by e.email
