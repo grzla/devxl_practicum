16562809084a0273d6839fb6860687e6ccec75f9
grzla <greg.j.lundberg@gmail.com>
2024-10-04 18:10:13 -0400

    09 could hosting instances performance statistics.sql

---
+++ 09_cloud_hosting_instances_performance_statistics.sql
@@ -0,0 +1,57 @@
+/*
+A cloud hosting provider dashboard that is in development requires a query that returns a list of networks where at least one instance within them has reached its maximum CPU usage threshold.
+
+The result should have the following columns: cidr | instances | avg_cpu_usage | avg_memory_usage | avg_network_usage.
+
+    cidr - network CIDR
+    instances - total number of instances for a specific network
+    avg_cpu_usage - average CPU usage of instances for a specific network
+        Record format is ##%, where the placeholders are ## in the order they appear:
+            Average CPU usage across all instances of a specific network, rounded up to the nearest integer
+    avg_memory_usage - average memory usage of instances for a specific network
+        Record format is ##%, where the placeholders are ## in the order they appear:
+            Average memory usage across all instances of a specific network, rounded up to the nearest integer
+    avg_network_usage - average network usage of instances for a specific network
+        Record format is ##%, where the placeholders are ## in the order they appear:
+            Average network usage across all instances of a specific network, rounded up to the nearest integer
+
+The result should be sorted in ascending order by cidr.
+
+Schema
+name	type	constraint	description
+networks			
+id	INT	PRIMARY KEY	Network ID
+cidr	VARCHAR(255)		IP Address v4 CIDR
+name	type	constraint	description
+instances			
+network_id	INT	FOREIGN KEY (network_id => networks.id)	Network ID
+cpu_usage	VARCHAR(255)		CPU usage
+memory_usage	VARCHAR(255)		Memory usage
+network_usage	VARCHAR(255)		Network usage
+
+Note:
+
+    The maximum CPU usage threshold is 80%.
+*/
+
+select
+    n.cidr,
+        -- subquery to select all instances
+        (select count(*) from instances i2 where i2.network_id = n.id) as instances, 
+    concat(ceil(avg(i.cpu_usage)), "%") as avg_cpu_usage,
+    concat(ceil(avg(i.memory_usage)), "%") as avg_memory_usage,
+    concat(ceil(avg(i.network_usage)), "%") as avg_network_usage
+from networks n
+join instances i
+    on n.id = i.network_id
+where 
+    n.id in (
+        -- subquery to find networks with at least one instance with cpu_usage >= 80
+        select distinct i.network_id
+        from instances i
+        where i.cpu_usage >= 80
+    )
+group by 
+    n.cidr, instances
+order by 
+    n.cidr
