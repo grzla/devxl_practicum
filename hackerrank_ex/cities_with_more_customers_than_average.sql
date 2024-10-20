44bd2b3d820bcd59e96ad3d715f077371dc47a75
grzla <greg.j.lundberg@gmail.com>
2024-10-20 12:53:22 -0400

    cities with more customers than average

--- hackerrank_ex/cities_with_more_customers_than_average.sql
+++ hackerrank_ex/cities_with_more_customers_than_average.sql
@@ -4,21 +4,22 @@ write a query which will return all cities with more customers than the average 
 
 
 select 
-    c.country_name, 
+    co.country_name, 
     ci.city_name, 
     count(cu.id) as number_of_customers
 from 
     city ci
 join 
-    country c on ci.country_id = c.id
+    country co on ci.country_id = co.id
+
 join 
     customer cu on ci.id = cu.city_id
 group by 
-    c.country_name, ci.city_name
+    co.country_name, ci.city_name
 having 
     count(cu.id) > (select avg(customer_count) 
                     from (select count(id) as customer_count 
                           from customer 
                           group by city_id) as avg_customers)
 order by 
-    c.country_name asc;
+    co.country_name asc;
