c636b09a0ed06e2db5626b3d19108f50dccbbbea
grzla <greg.j.lundberg@gmail.com>
2024-10-25 19:01:06 -0400

    top earners

---
+++ hackerrank_ex/top_earners.sql
@@ -0,0 +1,23 @@
+/*
+We define an employee's total earnings to be their monthly salary × months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
+
+| Column | Type |
+| employee_id | Integer |
+| name | String |
+| months | Integer |
+| salary | Integer |
+
+total_earnings = salary * months
+max
+*/
+
+select 
+    max(total_earnings) as max_earnings,
+    count(*) as count_max_earners
+from (
+    select 
+        employee_id,
+        salary * months as total_earnings
+    from employee
+) as earnings
+where total_earnings = (select max(salary * months) from employee);
