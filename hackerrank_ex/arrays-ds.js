4e1cfa9f65cd666fe9a678dc7ff848b2df568677
grzla <greg.j.lundberg@gmail.com>
2025-01-06 17:34:39 -0500

    arrays-ds (reverse array).js

---
+++ hackerrank_ex/arrays-ds.js
@@ -0,0 +1,60 @@
+/* https://www.hackerrank.com/challenges/arrays-ds/problem */
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray(a) {
+    // use spread operator to create new array since reverse() mutates original
+    return [...a].reverse()
+
+    /* alternative solution without using built-ins
+    
+    // create new array to avoid mutating input
+    let result = []
+    for (let i = a.length - 1; i >= 0; i--) {
+        result.push(a[i])
+    }
+    return result
+     */
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
