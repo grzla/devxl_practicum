e5967aa755c10b39d179ff3237fb49806d649a6f
grzla <greg.j.lundberg@gmail.com>
2025-04-17 08:32:49 -0400

    lc blind75 198 house robber js

--- leetcode/blind75/206 reverse linked list.py
+++ leetcode/blind75/206 reverse linked list.py
@@ -1,4 +1,4 @@
-/*
+"""
 
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
@@ -26,29 +26,21 @@ Constraints:
     The number of nodes in the list is the range [0, 5000].
     -5000 <= Node.val <= 5000
 
-*/
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head 
+        prev = None 
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var reverseList = function(head) {
-    let prev = null
-    let current = head
-    
-    while (current) {
-        let next = current.next
-        current.next = prev
-        prev = current
-        current = next
-    }
-    
-    return prev
-};
+        while cur: 
+            next = cur.next 
+            cur.next = prev 
+            prev = cur 
+            cur = next 
+
+        return prev 
