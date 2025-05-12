59a69b821af6081c1ff8dbf98a8fdff41fe75f57
grzla <greg.j.lundberg@gmail.com>
2025-05-12 09:38:48 -0400

    lc blind75 121 best time to buy and sell stock js

---
+++ leetcode/blind75/121 best time to buy and sell stock.js
@@ -0,0 +1,57 @@
+/* 
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=problem-list-v2&envId=oizxjoit
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+ 
+
+Constraints:
+
+    1 <= prices.length <= 105
+    0 <= prices[i] <= 104
+
+
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    // initialize variables to track minimum price and maximum profit
+    let minPrice = Infinity
+    let maxProfit = 0
+    
+    // iterate through each price
+    for (let price of prices) {
+        // update minimum price if we find a lower price
+        minPrice = Math.min(minPrice, price)
+        
+        // calculate potential profit if we sell at current price
+        let potentialProfit = price - minPrice
+        
+        // update maximum profit if we find a better deal
+        maxProfit = Math.max(maxProfit, potentialProfit)
+    }
+    
+    return maxProfit
+}
